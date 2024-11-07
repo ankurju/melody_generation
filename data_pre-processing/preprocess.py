1a616f2dbb4f4651e4d89b8f3cbb618d19e1f95e
Kumar Ankur <kr.ankur7@gmail.com>
2024-11-07 16:52:08 +0530

    created single file dataset

--- data_pre-processing/preprocess.py
+++ data_pre-processing/preprocess.py
@@ -1,7 +1,11 @@
 import os
 import music21 as m21
+import json
 
 KERN_DATASET_PATH = "deutschl/test"
+SINGLE_FILE_DATASET = "file_dataset"
+MAPPING_PATH = "mapping.json"
+SEQUENCE_LENGTH = 64
 SAVE_DIR = "dataset"
 ACCEPTABLE_DURATIONS=[
     0.25 , 0.5 , 0.75 , 1.0 , 1.5 , 2 , 3 , 4
@@ -25,7 +29,6 @@ def has_acceptable_durations(song , acceptable_durations):
     
     return True
 
-
 def transpose(song):
     """Transposes song to C maj/A min
 
@@ -41,8 +44,6 @@ def transpose(song):
     # estimate key using music21
     if not isinstance(key, m21.key.Key):
         key = song.analyze("key")
-
-    print(key)
 
     # get interval for transposition. E.g., Bmaj -> Cmaj
     if key.mode == "major":
@@ -74,12 +75,48 @@ def encode_song(song,time_step=0.25):
     encoded_song = " ".join(map(str,encoded_song))
     return encoded_song
 
+def load(file_path):
+    with open(file_path,"r") as fp:
+        song = fp.read()
+    return song
+
+def create_single_file_dataset(dataset_path,file_dataset_path,sequence_length):
+    new_song_delimiter = "/ " * sequence_length
+    songs = ""
+
+    # load encoded songs and add delimiters
+    for path,_,files in os.walk(dataset_path):
+        for file in files:
+            file_path = os.path.join(path,file)
+            song = load(file_path)
+            songs += song + " " + new_song_delimiter
+    songs = songs[:-1]
+
+    #save strings that contain all the dataset
+    with open(file_dataset_path,"w") as fp:
+        fp.write(songs)
+
+    return songs
+
+def create_mapping(songs,mapping_path):
+    mappings = {}
+
+    #identify the vocabulary
+    songs = songs.split()
+    vocabulary = list(set(songs))
+
+    #create mappings
+    for i,symbol in enumerate(vocabulary):
+        mappings[symbol] = i
+
+    #save the vocabulary to a json file
+    with open(mapping_path ,"w") as fp:
+        json.dump(mappings,fp,indent=4)
+
 def preprocess(dataset_path):
 
     # load the folk songs
-    print("Loading songs....")
     songs = load_songs_in_kern(dataset_path)
-    print(f"Loaded {len(songs)} songs")
 
     for i,song in enumerate(songs):
         # filter out songs that have non-acceptable durations
@@ -97,12 +134,10 @@ def preprocess(dataset_path):
         with open(save_path,"w") as fp:
             fp.write(encoded_song)
 
-
+def main():
+    preprocess(KERN_DATASET_PATH)
+    songs = create_single_file_dataset(SAVE_DIR,SINGLE_FILE_DATASET,SEQUENCE_LENGTH)
+    create_mapping(songs,MAPPING_PATH)
 
 if __name__== "__main__":
-    songs = load_songs_in_kern(KERN_DATASET_PATH)
-    print(f"Loaded {len(songs)} songs.")
-    song = songs[0]
-    preprocess(KERN_DATASET_PATH)
-    transposed_song = transpose(song)
-    # song.show()
+    main()
